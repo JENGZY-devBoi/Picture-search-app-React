78b64d405f8327a88d53b3f36f3b720ad8dec45e
Teerapong S <93139947+JENGZY-devBoi@users.noreply.github.com>
2022-10-04 23:08:12 +0700

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID sYf8r3Y_Tg_r4Q0XFOdx9uG1DVON4ALtagTuDhljqYI'
+        Authorization: 'KEY ID in splash API'
     }
 });
